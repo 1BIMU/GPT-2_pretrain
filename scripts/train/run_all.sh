9f523235983d3f22018d56c4df02f3aa13db6dd2
X1AOX1A <liyixia@me.com>
2026-03-10 01:47:36 +0000

    update file

--- scripts/train/run_all.sh
+++ scripts/train/run_all.sh
@@ -1,7 +1,9 @@
 source uv_gpt2/bin/activate
 
-
+export WANDB_PROJECT=wty_gpt2_pretrain
 export DATA_DIR=/mnt/yixiali/CODES/WTY/GPT-2_pretrain/data
 export OUTPUT_BASE=/mnt/yixiali/CODES/WTY/GPT-2_pretrain/output
 
-bash run_all.sh
+# bash train_with_dropout.sh --resume
+# bash train_without_dropout.sh --resume
+bash train_with_agd.sh
